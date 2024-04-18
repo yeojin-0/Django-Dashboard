b55684e9a6064fc0dae448ad35682fe0e70618ef
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 17:28:23 +0900

    Tag 기능 마무리

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from blog.models import Post
 from django.views.generic import ListView, DetailView, TemplateView # 추가 
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView
 from django.views.generic.dates import TodayArchiveView
+from django.conf import settings
 
 # ListView
 class PostLV(ListView):
@@ -15,6 +16,14 @@ class PostLV(ListView):
 class PostDV(DetailView):
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['disqus_short'] = f"{settings.DISQUS_SHORTNAME}"
+        context['disqus_id'] = f"post-{self.object.id}-{self.object.slug}"
+        context['disqus_url'] = f"{settings.DISQUS_MY_DOMAIN}{self.object.get_absolute_url()}"
+        context['disqus_title'] = f"{self.object.slug}"
+        return context
+        
 # ArchiveView
 class PostAV(ArchiveIndexView):
     model = Post
